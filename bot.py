b72e2ef2baba4f0b11fd45d8ad9b01ebe968f655
rlc21 <44525615+rlc21@users.noreply.github.com>
2023-03-26 15:07:19 -0400

    Add echo message for the bot.

--- bot.py
+++ bot.py
@@ -9,11 +9,21 @@ intents.message_content = True
 # read bot token from env file
 load_dotenv("TOKEN")
 token = os.environ.get("TOKEN")
+
 # create bot client
 client = discord.Client(intents=intents)
 
-def on_ready():
+@client.event
+async def on_ready():
     print(f"{client.user.name} logged in")
+
+@client.event
+async def on_message(message): 
+    if message.author.bot:
+        return
+    if message.content.startswith("/"):
+        cleanMsg = message.content[1:]
+        await message.channel.send("hihi :D -- " + cleanMsg + "")
 
 #log in
 client.run(token)
